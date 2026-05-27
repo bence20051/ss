b24e2143286baeedad0d05eb4814a96c67275a29
bence20051 <67710501+bence20051@users.noreply.github.com>
2026-05-27 23:29:26 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,108 +1,43 @@
-﻿using InformatikaiEszkozok_LIB.DATA;
-using InformatikaiEszkozok_LIB.MODEL;
+using InformatikaiEszkozok_LIB.DATA;
 
+var builder = WebApplication.CreateBuilder(args);
 
-Console.WriteLine("Adatbázis létrehozása indul...");
+// Add services to the container.
 
-using var context = new InfotermekDbContext();
-context.Database.EnsureDeleted(); // Ez a sor törli az adatbázist, ha már létezik, így mindig tiszta állapotból indulunk
-context.Database.EnsureCreated(); // Ez a sor létrehozza az adatbázist a modell alapján, ha még nem létezik
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-Console.WriteLine("Adatok beolvasása");
-Console.WriteLine();
+builder.Services.AddDbContext<InfotermekDbContext>(); // DbContext regisztrálása
 
-// ======================
-// 1. VÁSÁRLÓ BETÖLTÉS
-// ======================
-Console.WriteLine("Vásárlók beolvasása...");
-var vasarlok = File.ReadLines("vasarlo.csv") // Soronként olvassuk be a fájlt (memóriabarát)
-.Skip(1) // fejléc kihagyása
-.Select(line =>
-{                                      // Megtartja a pontos helyet, még ha üres is (pl. azonosító hiánya esetén)
-    var parts = line.Split(';', StringSplitOptions.None);
-
-    return new Vasarlo
+builder.Services.AddCors(options =>  // uj resz kell hogy html js fusson c# bckendel cors => letrehozs ez mskepp hibát dob cors rá
+{
+    options.AddPolicy("EngedjMindent", policy =>
     {
-        Azonosito = string.IsNullOrWhiteSpace(parts[1]) ? "" : parts[1], // Ha az azonosító hiányzik, akkor egy szóközt adunk helyette
-        Nev = parts[2],
-        Telefon = parts[3],
-        Email = parts[4]
-    };
-})
-.ToList();
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
-context.Vasarlo.AddRange(vasarlok);
-context.SaveChanges();
-Console.WriteLine();
 
-// ======================
-// 2. TERMÉK BETÖLTÉS
-// ======================
-Console.WriteLine("Termékek beolvasása...");
-var termekek = File.ReadLines("termek.csv") // Soronként olvassuk be a fájlt (memóriabarát)
-.Skip(1) // fejléc kihagyása
-.Select(line =>
-{                                      // Megtartja a pontos helyet, még ha üres is (pl. azonosító hiánya esetén)
-    var parts = line.Split(';', StringSplitOptions.None);
+var app = builder.Build();
 
-    return new Termek
-    {
-        Nev = parts[1],
-        Leiras = parts[2],
-        Egysegar = int.Parse(parts[3]),
-        Keszlet = int.Parse(parts[4]),
-        Elerheto = parts[5] == "1" ? true : false,
-        KepFajlnev = parts[6]
-    };
-})
-.ToList();
 
-context.Termek.AddRange(termekek);
-context.SaveChanges();
+app.UseCors("EngedjMindent");  // kellllllll 
 
-// ======================
-// 3. RENDELÉS BETÖLTÉS
-// ======================
-Console.WriteLine("Rendelések beolvasása...");
-var rendelesek = File.ReadLines("rendeles.csv") 
-.Skip(1) 
-.Select(line =>
-{                                     
-    var parts = line.Split(';', StringSplitOptions.None);
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-    return new Rendeles
-    {
-        VasarloId = int.Parse(parts[1]),
-        Datum = DateOnly.Parse(parts[2])
-    };
-})
-.ToList();
+app.UseHttpsRedirection();
 
-context.Rendeles.AddRange(rendelesek);
-context.SaveChanges();
-Console.WriteLine();
+app.UseAuthorization();
 
-// ======================
-// 4. RENDELÉS TÉTELEK BETÖLTÉS
-// ======================
-Console.WriteLine("Rendelések tételei beolvasása...");
-var rendelestetelek = File.ReadLines("rendeles_tetel.csv") 
-.Skip(1) 
-.Select(line =>
-{                                     
-    var parts = line.Split(';', StringSplitOptions.None);
+app.MapControllers();
 
-    return new RendelesTetel
-    {
-        RendelesId = int.Parse(parts[1]),
-        TermekId = int.Parse(parts[2]),
-        Mennyiseg = int.Parse(parts[3])
-    };
-})
-.ToList();
-
-context.RendelesTetel.AddRange(rendelestetelek);
-context.SaveChanges();
-Console.WriteLine();
-
-Console.WriteLine("Adatbázis létrehozva.");
+app.Run();
